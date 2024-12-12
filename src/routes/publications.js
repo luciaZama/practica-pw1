6caa3dac92f3cf990eea4f0d3d34767762a0a3cf
luciaZama <lzamanillol@gmail.com>
2024-12-12 21:03:49 +0100

    forgot await

--- src/routes/publications.js
+++ src/routes/publications.js
@@ -36,7 +36,7 @@ router.post('/publications/new-publication', async (req, res) => {
             });
           } else {
             const newPublication = new Publication({title, content});
-            newPublication.save();
+            await newPublication.save();
           }
     } catch (error) {
         console.error('An error occurred:', error);
