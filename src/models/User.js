11a9695ae19db6ea92e5968d2c350096190bf684
luciaZama <lzamanillol@gmail.com>
2024-12-14 20:28:48 +0100

    added errors messages to registration and did user's model

--- src/models/User.js
+++ src/models/User.js
@@ -1,2 +1,33 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    surnames: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,56 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/users/login', async (req, res) => {
+// GETS user login
+router.get('/users/login', (req, res) => {
     res.render('users/login');
 });
 
-router.get('/users/register', async (req, res) => {
+// GETS user registration
+router.get('/users/register', (req, res) => {
     res.render('users/register');
+});
+
+// POSTS user registration
+router.post('/users/register', async (req, res) => {
+    try {
+        const {username, name, surnames, email, password, confirm_password} = req.body;
+        const errors = [];
+        const info = [username, name, surnames, email];
+        const infoEsp = ['Nombre de usuario', 'Nombre', 'Apellidos', 'Email'];
+
+        for (let i = 0; i < info.length; i++) {
+            if (info[i].length <= 0) {
+                errors.push({text: 'Por favor, introduzca la información en el campo ' + infoEsp[i]});
+            }
+        }
+
+        if (password != confirm_password) {
+            errors.push({text: 'Las contraseñas no coinciden'});
+        }
+
+        if (password.length < 6) {
+            errors.push({text: 'La contraseña ha de tener 6 caracteres mínimo'});
+        }
+
+        if (errors.length > 0) {
+            res.render('users/register', {
+                errors, 
+                username, 
+                name, 
+                surnames, 
+                email, 
+                password, 
+                confirm_password
+            });
+        } else {
+
+        }
+    } catch {
+        console.error('An error occurred:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+    
 });
 
 module.exports = router;
