3ce71c55aa5825debc4422178959a77ae2387c6b
luciaZama <lzamanillol@gmail.com>
2024-12-14 20:40:15 +0100

    added encription methods to the password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -28,6 +29,16 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+UserSchema.methods.encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hash = bcrypt.hash(password, salt);
+    return hash;
+};
+
+UserSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
 
